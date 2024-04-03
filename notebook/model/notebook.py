0652010dbc80d99d542482ce502d3a98b3274514
PaulaS06 <menesesgasca@outlook.com>
2024-04-03 13:22:56 -0500

    .

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,1 +1,23 @@
+from dataclasses import dataclass, field
 from datetime import datetime
+
+@dataclass
+class Note:
+
+     creation_time: datetime = field(default_factory=datetime.now())
+     tags: list[str] = field(default_factory = [])
+
+    def __str__(self) -> str:
+        return f"Code: { }\nCreation Creation date: {self.creation_time}\nCreation Title: {}"
+
+    def add_tag (self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+
+class Notebook:
+    def __init__(self):
+    def add_note (self, nota):
+
+
+
