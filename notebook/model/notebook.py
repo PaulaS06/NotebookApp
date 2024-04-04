83000e132a3cd206e7655ac67efbd18e4134fd65
PaulaS06 <menesesgasca@outlook.com>
2024-04-03 22:02:45 -0500

    .

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,23 +1,53 @@
 from dataclasses import dataclass, field
 from datetime import datetime
+from random import random
+
 
 @dataclass
 class Note:
+     code:int
+     title:str
+     text:str
+     importance:str
 
-     creation_time: datetime = field(default_factory=datetime.now())
-     tags: list[str] = field(default_factory = [])
+     creation_date: datetime = field(default_factory=datetime.now)
+     tags: list[str] = field(default_factory=list)
+
+     HIGH: str = "HIGH"
+     MEDIUM: str = "MEDIUM"
+     LOW: str = "LOW"
+
 
     def __str__(self) -> str:
-        return f"Code: { }\nCreation Creation date: {self.creation_time}\nCreation Title: {}"
+        return f"Code: {self.code}\nCreation date: {self.creation_date}\n{self.title}: {self.text}"
 
-    def add_tag (self, tag):
+    def add_tag (self, tag:str):
         if tag not in self.tags:
             self.tags.append(tag)
 
-
+@dataclass
 class Notebook:
-    def __init__(self):
-    def add_note (self, nota):
+    notas = dict()
 
+    def add_note (self, title:str, text:str, importance:str):
+        nota = Note(title, text, importance)
+        codigo = random.randint(1000,9999)
+        self.notas[codigo] = nota
+        return codigo
+
+    def important_notes (self) -> list[Note]:
+        result = []
+        for codigo in self.notas:
+            if self.notas[codigo].importance == "HIGH" or self.notas[codigo].importance == "MEDIUM":
+                result.append(self.notas[codigo])
+        return result
+
+
+    def tags_notes_count (self) -> dict[str, int]:
+        tags = {}
+        for codigo in self.notas:
+            for tag in self.notas[codigo].tags:
+                tags[tag] += 1
+        return tags
 
 
